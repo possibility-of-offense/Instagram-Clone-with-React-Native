986787f9963f3112501204b4042f781b7fa20ec8
Венцислав Илиев <ultimatemeaning90@gmail.com>
2023-01-15 17:23:00 +0200

    Init

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import Constants from "expo-constants";
+import { Formik } from "formik";
+
+// Own dependecies
+import AppInput from "./components/Form/Input";
+import Logo from "./components/Logo/Logo";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Logo />
+      <View style={styles.form}>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <>
+              <AppInput
+                autoCorrect={false}
+                autoComplete={false}
+                name="email"
+                onChange={handleChange("email")}
+                placeholder="Enter email"
+                value={values.email}
+              />
+
+              <AppInput
+                autoCorrect={false}
+                autoComplete={false}
+                name="password"
+                onChange={handleChange("password")}
+                placeholder="Enter password"
+                secureTextEntry
+                value={values.password}
+              />
+              <Button title="Click" onPress={handleSubmit} />
+            </>
+          )}
+        </Formik>
+      </View>
     </View>
   );
 }
@@ -13,8 +47,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight + 10,
+    marginHorizontal: 10,
+    textAlign: "center",
+    alignItems: "center",
+  },
+  form: {
+    paddingVertical: 20,
+    width: "80%",
   },
 });
---
+++ components/Form/Input.js
@@ -0,0 +1,37 @@
+import { Text, TextInput, StyleSheet } from "react-native";
+
+const AppInput = ({ error = undefined, onChange, value, ...others }) => {
+  return (
+    <>
+      <TextInput
+        style={[
+          styles.input,
+          {
+            marginBottom: error ? 3 : 10,
+          },
+        ]}
+        onChangeText={onChange}
+        value={value}
+        {...others}
+      />
+      {error && <Text style={styles.error}>Some error</Text>}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    paddingVertical: 4,
+    paddingHorizontal: 12,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 3,
+    marginBottom: 10,
+  },
+  error: {
+    color: "red",
+    marginBottom: 10,
+  },
+});
+
+export default AppInput;
